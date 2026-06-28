8292455a0c71a87d9f724e4a7fb43235809378a5
Tarun <tarunchakratejavalluri@gmail.com>
2026-06-28 22:16:42 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (string &word : wordDict) {
+                int len = word.size();
+
+                if (i + len <= n && s.substr(i, len) == word) {
+                    dp[i] = dp[i + len];
+                }
+
+                if (dp[i])
+                    break;
+            }
+        }
+
+        return dp[0];
+    }
+};
