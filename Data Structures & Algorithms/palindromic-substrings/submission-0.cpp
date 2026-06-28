dd7a8e752ac92f4ffcffbd4293e1dcb7223ec36a
Tarun <tarunchakratejavalluri@gmail.com>
2026-06-28 22:13:09 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Odd-length palindromes
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+
+            // Even-length palindromes
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+        }
+
+        return count;
+    }
+};
